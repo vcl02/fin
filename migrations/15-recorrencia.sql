65fdf7b129c84aa8a798b47b6aaeb5fdbb7261af
vcl02 <78442423+vcl02@users.noreply.github.com>
2026-09-23 12:56:22 -0300

    .

---
+++ migrations/15-recorrencia.sql
@@ -0,0 +1,12 @@
+ALTER TABLE fin
+ADD COLUMN recorrencia_id integer;
+
+CREATE SEQUENCE fin_recorrencia_seq;
+
+SELECT setval(
+    'fin_recorrencia_seq',
+    COALESCE(
+        (SELECT MAX(recorrencia_id) FROM fin),
+        0
+    )
+);
